c469ddf88762094720eb04e1357fca7422dbd7a2
CSL-ctc-user <rana.takano@ctc-g.co.jp>
2026-05-21 09:56:05 +0900

    Update hf_proxy_train.py

--- security_tests/hf_proxy_train.py
+++ security_tests/hf_proxy_train.py
@@ -67,14 +67,16 @@ training_args = TrainingArguments(
     report_to="none",
 )
 
+
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    tokenizer=tokenizer,
+    processing_class=tokenizer,
     data_collator=data_collator,
 )
+
 
 print("Start training...")
 trainer.train()
